f1520b2975e4746e88633db47a7ffd230c443108
Mat Janson Blanchet <mat.jb@jansensan.net>
2015-06-24 11:16:38 -0700

    Added HomeCopy module

---
+++ client/app/components/home/home-copy/home-copy.js
@@ -0,0 +1,18 @@
+import angular from 'angular';
+import template from './home-copy.html';
+
+// FIXME: figure out why not showing
+
+let homeCopyModule = angular
+  .module('homeCopy', [])
+  .directive('homeCopy', HomeCopy);
+
+export {homeCopyModule};
+
+function HomeCopy() {
+  let homeCopy = {
+    template,
+    restrict: 'E'
+  };
+  return homeCopy;
+}
--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -2,28 +2,29 @@ import './home.css';
 import angular from 'angular';
 import template from './home.html';
 import {HomeController as controller} from './home.controller';
+import {homeCopyModule} from './home-copy/home-copy';
 
 // no issue, webpack handles duplication and circular refs
 
 // create var to export
 let homeModule = angular
-  .module('home', [])
+  .module('home', getDependencies())
   .directive('home', Home);
 
 // export for derefencing (todo: read more about this)
 export {homeModule};
 
 function Home() {
-  let _home = {
+  let home = {
     template,
     controller,
     restrict: 'E'
   };
-  return _home;
+  return home;
 }
 
 function getDependencies() {
   return [
-    controller.name
+    homeCopyModule.name
   ];
 }
